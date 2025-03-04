115432421896813729853457e07b192b7fad8298
Olmichu22 <olmichu33@gmail.com>
2025-03-04 11:23:27 +0100

    Create parent particle class, recoparticle and genparticle

--- modules/ParticleObjects.py
+++ modules/ParticleObjects.py
@@ -1,31 +1,159 @@
 import ROOT
 
-class GenParticle:
-  """ Particle class to store the information of a particle.
+class Particle():
+  """ Standard particle class to store the information of a particle.
   Attributes:
       p4 (TLorentzVector): 4-momentum of the particle.
-      ID (int): PDG ID of the particle.
+      PDGID (int): PDG ID of the particle.
+      ID (int): Decay ID of the particle.
       charge (int): Charge of the particle.
-      maxAngle (float): Maximum angle between the constituents of the particle.
-      nConst (int): Number of constituents of the particle.
-      const (dict): Dictionary with the constituents of the particle.
   """
   
-  def __init__(self, visP4=None, ID=-1, charge=0, genP4=None, maxAngleConsts=0, nConsts=0, const=None):
+  def __init__(self, p4=None, PDGID=-1, ID=-1, charge=0):
     """ Constructor of the Particle class."""
-    self.visp4 = visP4 if visP4 is not None else ROOT.TLorentzVector(0, 0, 0, 0)
+    self.p4 = p4 if p4 is not None else ROOT.TLorentzVector(0, 0, 0, 0)
+    self.PDGID = PDGID
     self.ID = ID
     self.charge = charge
-    self.p4 = genP4 if genP4 is not None else ROOT.TLorentzVector(0, 0, 0, 0)
+  
+  def getPDG(self):
+    return self.PDGID
+  
+  def getID(self):
+    return self.ID
+  
+  def getCharge(self):
+    return self.charge
+  
+  def getMomentum(self):
+    return self.p4
+  
+  def getMass(self):
+    return self.p4.M()
+  
+  def setPDG(self, PDGID):
+    self.PDGID = PDGID
+    
+  def setID(self, ID):
+    self.ID = ID
+  
+  def setCharge(self, charge):
+    self.charge = charge
+  
+  def setMomentum(self, p4):
+    self.p4 = p4
+  
+  def ShowInfo(self):
+    """ Print the information of the particle."""
+    print("Particle PDG: %d, ID: %d, Charge: %d, Mass: %f" % (self.PDGID, self.ID, self.charge, self.p4.M()))
+    
+  def __str__(self):
+    return "PDG: %d, ID: %d, Charge: %d, Mass: %f" % (self.PDGID, self.ID, self.charge, self.p4.M())
+
+class RecoParticle(Particle):
+  """ Particle class to store the information of a reco level particle.
+  Attributes:
+      p4 (TLorentzVector): 4-momentum of the particle.
+      ID (int): Decay ID of the particle.
+      charge (int): Charge of the particle.
+      maxCone (float): Maximum angle between the guide particle and the rest of the constituents.
+      nConst (int): Number of constituents of the particle.
+      const (dict): Dictionary with the constituents of the particle.
+      PDGID (int):  PDG ID of the particle.
+  """
+  
+  def __init__(self, p4=None, ID=-1, charge=0, maxCone=0, nConst=0, const=None, PDGID=-1):
+    """ Constructor of the Particle class."""
+    # Initialize the parent class
+    super(RecoParticle, self).__init__(p4, PDGID, ID, charge)
+    
+    self.maxCone = maxCone
+    self.nConst = nConst
+    self.const = const if const is not None else {}
+  
+  def getMomentum(self):
+    return self.p4
+  
+  def getMass(self):
+    return self.p4.M()
+  
+  def getID(self):
+    return self.ID
+  
+  def getCharge(self):
+    return self.charge  
+  
+  def getMaxCone(self):
+    return self.maxCone
+
+  def getnConst(self):
+    return self.nConst
+  
+  def getDaughters(self):
+    return self.const
+  
+  def setMomentum(self, p4):
+    self.p4 = p4
+  
+  def setID(self, ID):
+    self.ID = ID
+  
+  def setCharge(self, charge):
+    self.charge = charge
+  
+  def setMaxCone(self, maxCone):
+    self.maxCone = maxCone
+  
+  def setDaughters(self, const):
+    self.const = const
+    self.nConst = len(const)
+  
+  def addDaughter(self, daughter):
+    self.const[self.nConst] = daughter
+    self.nConst += 1
+  
+  def ShowInfo(self):
+    """ Print the information of the particle and its constituents."""
+    print("RecoParticle ID: %d, Charge: %d, Mass: %f, nConst: %d, Angle: %d" % (self.ID, self.charge, self.p4.M(), self.nConst, self.maxCone))
+    for i in range(self.nConst):
+      print("  Constituent %d:" % i)
+      print(self.const[i])
+  
+
+class GenParticle(Particle):
+  """ Particle class to store the information of a gen level particle.
+  Attributes:
+      visp4 (TLorentzVector): 4-momentum of the visible part of the particle.
+      ID (int): Decay ID of the particle.
+      charge (int): Charge of the particle.
+      visCharge (int): Charge of the visible part of the particle.
+      genP4 (TLorentzVector): 4-momentum of the particle.
+      maxAngleConsts (float): Maximum angle between the constituents of the particle.
+      nConst (int): Number of constituents of the particle.
+      const (dict): Dictionary with the constituents of the particle.
+      PDGID (int):  PDG ID of the particle.
+  """
+  
+  def __init__(self, visP4=None, ID=-1, charge=0, visCharge = 0, genP4=None, maxAngleConsts=0, nConsts=0, const=None, PDGID=-1):
+    """ Constructor of the Particle class."""
+    # Initialize the parent class
+    super(GenParticle, self).__init__(genP4, PDGID, ID, charge)
+    
+    self.visp4 = visP4 if visP4 is not None else ROOT.TLorentzVector(0, 0, 0, 0)
+    self.visCharge = visCharge
     self.maxAngle = maxAngleConsts
     self.nConst = nConsts
     self.const = const if const is not None else {}
+    
     
   def getID(self):
     return self.ID
 
   def getCharge(self):
     return self.charge
+  
+  def getvisCharge(self):
+    return self.visCharge
   
   def getMomentum(self):
     return self.p4
@@ -53,6 +181,9 @@ class GenParticle:
   
   def setCharge(self, charge):
     self.charge = charge
+    
+  def setvisCharge(self, visCharge):
+    self.visCharge = visCharge
   
   def setMomentum(self, candP):
     self.p4.SetXYZM(candP.getMomentum().x,
@@ -79,10 +210,10 @@ class GenParticle:
     
   def ShowInfo(self):
     """ Print the information of the particle and its constituents."""
-    print("GenParticle ID: %d, Charge: %d, Mass: %f, VisMass: %f, nConst: %d, Angle: %d" % (self.ID, self.charge, self.p4.M(), self.visp4.M(), self.nConst, self.maxAngle))
+    print("GenParticle ID: %d, Charge: %d, Mass: %f, VisMass: %f, nConst: %d, Angle: %d" % (self.ID, self.visCharge, self.p4.M(), self.visp4.M(), self.nConst, self.maxAngle))
     for i in range(self.nConst):
       print("  Constituent %d:" % i)
       print(self.const[i])
   
   def __str__(self):
-    return "ID: %d, Charge: %d, Mass: %f, VisMass: %f, nConst: %d, Angle: %d" % (self.ID, self.charge, self.p4.M(), self.visp4.M(), self.nConst, self.maxAngle)
+    return "ID: %d, Charge: %d, Mass: %f, VisMass: %f, nConst: %d, Angle: %d" % (self.ID, self.visCharge, self.p4.M(), self.visp4.M(), self.nConst, self.maxAngle)
